aa2e4caba90b26be34c41fbf833f4b71d6981f2b
audraybinet <audreybinet@yahoo.fr>
2020-01-09 16:41:51 +0100

    push + ajoutjs + css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-// 1. Create the button
+// 1. je crée le boutton
 var button = document.createElement("button");
-button.innerHTML = "ENVOYER";
+button.innerHTML = "send";
 
-// 2. Append somewhere
+// 2. je l'nvoie
 var body = document.getElementsByTagName("form")[0];
 body.appendChild(button);
 
-// 3. Add event handler
+// 3. je fais une alert pour verifié que ca fonction
 button.addEventListener ("click", function() {
-  alert("putain");
+  alert("aller");
 });
--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php include 'controlformulaire.php'; ?>
+<?php
+if (!empty($_GET['param'])) {
+    $param = $_GET['param'];
+} else {
+    $param = 0;
+}
+$dom = $exp = $element = $enfants = $nom = $title =  $affiche = '';
+if (file_exists('source.xml')) {
+    $affiche = true;
+    $dom = new DomDocument; //création objet dom
+    $dom->load("source.xml"); //chargement du fichier xml
+    $exp = $dom->getElementsByTagName('page'); //chargement des pages
+    $element = $exp->item($param); // On obtient le noeud de la page
+    $enfants = $element->childNodes; //réccupération des éléments enfants
+    foreach ($enfants as $enfant) { // On prend chaque noeud enfant séparément.
+        $nom = $enfant->nodeName; // On prend le nom de chaque noeud.
+        if ($nom == 'title') {
+            $title = $enfant->nodeValue; //réccupération du titre
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
     <head>
@@ -5,7 +28,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="assets/css/style.css" type="text/css" rel="stylesheet" />
-        <title>Maçonnerie Ocordo</title>
+        <title><?= $title ?></title>
     </head>
     <body>
 
@@ -23,31 +46,19 @@
 
                 <div id="exercicesContent" class="ml-5 mb-3 mt-3 d-flex flex-column align-items-center justify-content-center col-10">
                     <?php
-                    if (!empty($_GET['param'])) {
-                        $param = $_GET['param'];
-                    } else {
-                        $param = 0;
+                    if ($affiche) {
+                        //$element = $exp->item($param); // On obtient le noeud de la page
+                        //$enfants = $element->childNodes; // On récupère les noeuds enfants du noeud de la page
+                        foreach ($enfants as $enfant) { // On prend chaque noeud enfant séparément.
+                            $nom = $enfant->nodeName; // On prend le nom de chaque noeud.
+                            echo $enfant->nodeValue;
+                        }
                     }
-
-                    $dom = new DomDocument;
-                    $dom->load("source.xml");
-                    $exp = $dom->getElementsByTagName('page');
-
-                    $element = $exp->item($param); // On obtient le noeud de la page
-                    $enfants = $element->childNodes; // On récupère les noeuds enfants du noeud de la page
-                    foreach ($enfants as $enfant) { // On prend chaque noeud enfant séparément.
-                        $nom = $enfant->nodeName; // On prend le nom de chaque noeud.
-                        echo $enfant->nodeValue;
-                    }
-
                     ?>
                 </div>
             </div>
+             <?php
+            include 'footer.php';
+            ?>
         </div>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        <script src="assets/js/script.js"></script>
-    </body>
-</html>
-
+        
